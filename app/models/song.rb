904972c4328eae3af364b77c38b005a2ca17fc5c
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-12-17 20:41:43 -0500

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ApplicationRecord
+  validates :title, presence: true
+  validate :only_one_song
+  validates :release_year, presence: true, if: :released
+  validates :release_year, numericality: {less_than_or_equal_to: Time.now.year}
+  validates :artist_name, presence: true
+
+  def only_one_song
+    if Song.where.not(id: self.id).where('release_year = ?', self.release_year).any? {|song| song.artist_name == self.artist_name}
+      errors.add(:artist_name, "Cannot release second song in the same year")
+    end
+  end
+
+
+  Song.where
+end
